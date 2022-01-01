b2b218206166b875ce4503f83957fdb9bc442af6
SeungWon <20sunrin041@sunrint.hs.kr>
2022-01-02 04:16:50 +0900

    refactor: auth service schema & error handle logic

--- src/resolvers/app/auth/Mutationt.ts
+++ src/resolvers/app/auth/Mutationt.ts
@@ -1,6 +1,5 @@
 import { Context } from "config"
 import { env } from "config/env"
-import { ApolloError } from "apollo-server-express"
 import jwt from "jsonwebtoken"
 import { MutationLoginArgs } from "config/models"
 
@@ -8,13 +7,16 @@ export const login = async (parent: void, args: MutationLoginArgs, context: Cont
 	const { id, password } = args.input
 	const user = await context.db.collection("user").findOne({ id, password })
 	if (user === null) {
-		throw new ApolloError("아이디 혹은 비밀번호가 잘못되었습니다", undefined, {
-			action: "로그인",
-			message: `아이디 혹은 비밀번호 오류 (아이디 : ${id})`,
-		})
+		return {
+			__typename: "InvalidAccountError",
+			message: "아이디 혹은 비밀번호가 잘못되었습니다",
+			path: "login",
+			suggestion: "계정을 다시 확인해주세요",
+		}
 	}
 	const token = jwt.sign({ role: user.role, id: id }, env.JWT_SECRET)
 	return {
+		__typename: "User",
 		role: user.role,
 		token,
 	}
